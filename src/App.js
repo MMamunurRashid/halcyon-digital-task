6308c98b72e71f2ca71879913b23a29e454e1d6d
MMamunurRashid <mdmamun.iubat.m@gmail.com>
2024-06-03 01:20:11 +0600

    navbar and top ads added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import Navbar from "./Components/Navbar/Navbar";
+import './App.css'
+import { useState } from "react";
 
 
 function App() {
+  const [isAdVisible, setIsAdVisible] = useState(true);
+
+  const closeAd = () => {
+    setIsAdVisible(false);
+  };
   return (
     <div className="">
-      <h1>Hello world</h1>
+       {isAdVisible && (
+        <div className="ad-banner">
+          <span className="ad-content">Sign up and get 20% off to your first order. <a href="/">Sign Up Now </a></span>
+          <span className="ad-close" onClick={closeAd}>&times;</span>
+        </div>
+      )}
+      
+    <div className="app">
+    <Navbar/>
+    </div>
     </div>
   );
 }
